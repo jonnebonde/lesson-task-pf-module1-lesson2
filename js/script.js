3d018915d142a1f893f3597e596c953a00fb3f0a
jonnebonde <jonnebonde@gmail.com>
2021-11-28 15:48:47 +0100

    enter

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/* Question 1 */
+
+var integer = 10;
+var decimal = 1.1;
+
+/* Question 2 */
+
+var daysOfTheWeek = "Monday" + "-" + "Thuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday"
+
+console.log(daysOfTheWeek);
+
+/* Question 3 */
+
+var addition = 10 + 2;
+var subtraction = 10 - 2;
+var multiplication = 10 * 2;
+var division = 10 / 2;
+
+console.log(addition, subtraction, multiplication, division);
+
+/* Question 4 */
+
+var boolean = false;
+
+/* Question 5 */
+
+var Frogs = "The frogs are angry.";
+
+console.log(Frogs.length);
+
+/* Questions 6 */
+
+var timeElapsed;
+var totalPrice;
+
+/* Question 7 */
+
+var number1 = 53;
+var number2 = 44.6;
+
+/* Question 8 */
+
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+console.log(typeof pet)
+console.log(typeof age)
+console.log(typeof discountApplied)
+console.log(timeLeft)
+
+
+/* Question 9 */
+
+var headingText = "Updating heading";
+
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+/* Question 10 */
+
+var body = document.querySelector("body")
+body.style.backgroundColor = "lightskyblue";
